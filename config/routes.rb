f97783a57e974c8cd73069fbcf7823814d0d23f9
Rick Smoke <rsmoke@umich.edu>
2022-05-17 15:38:16 -0400

    the stuff

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
+  resources :users
+  root 'rooms#index'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+end
